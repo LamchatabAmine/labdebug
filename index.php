99bc08cb50fb8287c9129da4fce6202e7576a82a
amine.lamchatab <115371227+LamchatabAmine@users.noreply.github.com>
2023-10-11 15:40:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+`<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "admin";
+$dbname = "labdebugage";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `stagiaires`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Full Name</th><th>Email</th></tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['email'] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+`
